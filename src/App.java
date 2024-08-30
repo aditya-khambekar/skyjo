eea7cb3e0ad493503536ed8b505bfe4186e5f37c
Aditya <adityakhambekar9@gmail.com>
2024-08-30 16:18:45 -0500

    add images

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
-public class App {
-    
+import javax.swing.JFrame;
+
+public class App extends JFrame{
+    public static void main(String[]args){
+        App app = new App("Skyjo");
+        app.setSize(1600, 1000);
+        app.setVisible(true);
+    }
+
+    public App(String framename){
+        super(framename);
+    }
 }
